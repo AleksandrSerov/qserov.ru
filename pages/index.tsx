04c4f3c66969850c4fe69b4a2f490e20ad43f94a
Aleksandr Serov <sashaserov2010@gmail.com>
2022-06-29 21:28:04 +0300

    feat(*): add home page for test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,11 @@ import { Input } from '../components/input';
 
 import styles from './index.module.css';
 
-const HomePage: React.FC = () => (
+const Index: React.FC = () => (
 	<div className={ styles.testClass }>
 		<h1>Hello world</h1>
 		<Input className='t' foo={ true } />
 	</div>
 );
 
-export default HomePage;
+export default Index;
