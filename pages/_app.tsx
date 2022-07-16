966fd06a8cfd3fb99d2d35cdf558db68b189887e
Aleksandr Serov <sashaserov2010@gmail.com>
2022-07-16 18:57:35 +0300

    feat(*): add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,30 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-	<NextUIProvider>
-		<Component { ...pageProps } />
-	</NextUIProvider>
+	<React.Fragment>
+		<Script
+			strategy='lazyOnload'
+			src={ `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}` }
+		/>
+
+		<Script strategy='lazyOnload'>
+			{`
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+		page_path: window.location.pathname,
+		});
+	`}
+		</Script>
+		<NextUIProvider>
+			<Component { ...pageProps } />
+		</NextUIProvider>
+	</React.Fragment>
+
 );
 
 export default MyApp;
